e78e7ce7baa4d6c3bd3aa6d17ddc48b459c9d8ec
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-06-07 17:24:22 +0530

    something something

--- components/home.tsx
+++ components/home.tsx
@@ -13,6 +13,7 @@ import {
 import {motion} from 'framer-motion'
 import Link from 'next/link';
 import { Shrikhand } from '@next/font/google'
+import Kavan from '../public/ka1.png'
 
 const advent = Shrikhand({
     subsets:['latin'],
@@ -36,12 +37,13 @@ const _variants = {
 export default function Home() {
     return (
         <div className={advent.className}>
+            <div className='md:flex md:gap-10 items-center justify-between'>
         <motion.section
             initial={{opacity:0, y:-40}}
             animate={{opacity:1, y:0}}
             transition={{delay:0.2,duration:1}}
             id="home"
-            className=' px-10 sm:p-10 rounded-xl text-left sm:pt-24 pt-10 overflow-hidden pb-12'>
+            className=' px-10 sm:p-10 rounded-xl text-left sm:pt-24 pt-10 overflow-hidden pb-12 z-10'>
             <p className='text-xl text-orange-600 font-bold pb-1 font-mono'>{"Hi, my name is"}</p>
             <h1 className='text-5xl 2xl:text-8xl xl:text-7xl pb-1 text-white md:text-7xl lg:text-7xl'>Kavan Thakkar</h1>
             <h2 className='text-2xl pt-1 pb-5 md:text-3xl xl:text-4xl font-bold text-gray-300 font-mono'>{"I am a Full Stack Developer and a Designer,"}</h2>
@@ -61,6 +63,10 @@ export default function Home() {
             </motion.button>
             </Link>
         </motion.section>
+        <div className=''>
+            <Image src={Kavan} layout='cover' quality={100} alt='Image of the creator' className=' rounded-full bg-white bg-opacity-5 border-white/5 border-2 shadow-xl shadow-white/5'/>
+        </div>
+        </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Home from '../../components/home'
 import {motion as m} from 'framer-motion'
+import Image from 'next/image'
+import Kavan from '../../public/ka1.png'
 
 
 export default function app() {
   return (
-          <div className=' px-5 md:px-20 lg:px-40 2xl:px-80 sm:py-10 xl:pb-36 min-h-screen 2xl:max-h-screen bg-gradient-to-b from-black via-gray-900 to-sky-900/70 justify-center'>
+          <div className=' px-5 md:px-20 lg:px-10 2xl:px-40 sm:py-10 xl:pb-36 min-h-screen 2xl:max-h-screen bg-gradient-to-b from-black via-gray-900 to-sky-900/70 justify-center'>
             <Home />
             {/* <About /> */}
           </div>
